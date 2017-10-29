35d227e9319a2d722b843ea49e0c087471a6cc6e
Karthik4293 <karthik4293@gmail.com>
2017-10-28 20:44:58 -0400

    trivial mac -- non working

--- mvm3_part1.sv
+++ mvm3_part1.sv
@@ -29,10 +29,10 @@ endmodule
 
 module control(clk, s_valid, s_ready, m_ready, m_valid, reset, addr_x, wr_en_x, addr_a, wr_en_a, done, clear_acc, addr_y, wr_en_y, addr_z, wr_en_z);
 
-       input clk, s_valid, reset, s_ready;
+       input clk, s_valid, reset, m_ready;
        output logic [3:0] addr_a;
 	     output logic [1:0] addr_x, addr_y, addr_z;
-       output logic done, wr_en_x, wr_en_a, clear_acc, wr_en_y, wr_en_z,m_valid, m_ready;
+       output logic done, wr_en_x, wr_en_a, clear_acc, wr_en_y, wr_en_z,m_valid, s_ready;
        logic write_done_x, write_done_a, mac_done, write_done_y, write_done_z;
 	     logic [2:0] state, next_state;
 	     logic [1:0] multiplier, counter;
@@ -61,12 +61,12 @@ module control(clk, s_valid, s_ready, m_ready, m_valid, reset, addr_x, wr_en_x, 
                 else
                     next_state = 1;
 
-            2 : if (write_done_x == 1)          // State 2 starts writing into the memory and checks if write is done
+            2 : if (write_done_a == 1)          // State 2 starts writing into the memory and checks if write is done
                     next_state = 3;
                 else
                     next_state = 2;
 
-            3 : if (write_done_a == 1)          // State 3 -- same as above
+            3 : if (write_done_x == 1)          // State 3 -- same as above
                     next_state = 4;
                 else
                     next_state = 3;
@@ -83,20 +83,26 @@ module control(clk, s_valid, s_ready, m_ready, m_valid, reset, addr_x, wr_en_x, 
           endcase
        end
 
-       assign wr_en_x = (state == 2);
-       assign wr_en_a = (state == 3);
-       assign wr_en_y = (((counter==1)|(mac_done==1))&(state==4));
-    //   assign m_valid = (addr_y>=0)&(addr_y<=2);
+       assign wr_en_a = (state == 2);
+       assign wr_en_x = (state == 3);
+       assign wr_en_y = (state == 4);
+       assign m_valid = (addr_y>=0)&(addr_y<=2);
 
     //   assign display_done = ((state==5)&(addr_y==2)&(entry==1));
 
 
 
-       assign write_done_x = ((state==2)&(addr_x==2));
-       assign write_done_a = ((state==3)&(addr_a==8));
+       assign write_done_x = ((state==3)&(addr_x==2));
+       assign write_done_a = ((state==2)&(addr_a==8));
        assign write_done_y = ((state==4)&(addr_y==2));
        assign write_done_z = ((state==5)&(addr_z==2));
 
+      always_ff @(posedge clk) begin
+           if ((addr_a<=8)&&(addr_x<=2))
+              s_ready <= 1;
+           else
+              s_ready <= 0;
+      end
 
 
       always_ff @(posedge clk) begin
@@ -110,20 +116,21 @@ module control(clk, s_valid, s_ready, m_ready, m_valid, reset, addr_x, wr_en_x, 
            else if (state == 1) begin
                addr_a <= 0;
                addr_x <= 0;
+               addr_y <= 0;
             end
 
             else if (state == 2) begin
-                 if (write_done_x == 0)
-                     addr_x <= addr_x + 1;
+                 if ((write_done_a == 0) && (addr_a<=8))
+                     addr_a <= addr_a + 1;
                  else
-                     addr_x <= 0;
+                     addr_a <= 0;
             end
 
             else if (state == 3) begin
-                 if (write_done_a == 0)
-                    addr_a <= addr_a + 1;
+                 if ((write_done_x == 0) && (addr_x<=2))
+                    addr_x <= addr_x + 1;
                  else
-                    addr_a <= 0;
+                    addr_x <= 0;
             end
 
             else if (state == 4) begin
@@ -149,6 +156,7 @@ module control(clk, s_valid, s_ready, m_ready, m_valid, reset, addr_x, wr_en_x, 
 
             else if (state == 5) begin
                   if (m_valid & m_ready & (addr_y<=2)) begin
+                      wr_en_z <= 1;
                       addr_y <= addr_z;
                       addr_y <= addr_y + 1;
                   end
@@ -193,7 +201,7 @@ module memory(clk, data_in, data_out, addr, wr_en);
 	input clk, wr_en;
 	logic signed [SIZE-1:0][WIDTH-1:0] mem;
 	always_ff @(posedge clk) begin
-		data_out <= mem[addr];
+		 data_out <= mem[addr];
 		if (wr_en)
 			mem[addr] <= data_in;
 	end
@@ -219,11 +227,14 @@ module part2_mac(clk, clear_acc, a, b, f, overflow);
 
 // Conditions on reset
         always_ff @(posedge clk) begin
-            if (clear_acc == 1)
+            if (clear_acc == 1) begin
                   f <=0;
-            else
+                  overflow <= 0;
+            end
+            else  begin
                   f<=add;
                   overflow <= overflow_int;
+            end
         end
 
         always_comb  begin
@@ -273,12 +284,12 @@ module check_timing();
 
       logic [15:0] j;
 
-      always_comb begin
+      always @* begin
          if (s_valid == 1) begin
+            $display("im here");
             data_in = invals[j];
-            s_ready = 1;
-         end
-          else
+          end
+         else
             data_in = 'x;
       end
 
@@ -286,27 +297,37 @@ module check_timing();
       always_comb begin
          if ((j>=0) && (j<12) ) begin
             s_valid=1;
+            $display("im here at s_valid");
          end
          else
             s_valid=0;
       end
 
       always @(posedge clk) begin
-         if (s_valid && s_ready)
+         if (s_valid && s_ready)begin
             j <= #1 j+1;
+            $display("S_valid and S_ready asserted");
+            end
       end
 
-      logic [15:0] i;
-      always @* begin
-         if ((i>=0) && (i<3) && (rb2==1'b1))
+     logic [15:0] i;
+    always @* begin
+         if ((i>=0) && (i<3) && (rb2==1'b1)) begin
             m_ready = 1;
-         else
+            $display("M_ready is set");
+        end
+         else begin
             m_ready = 0;
+            $display("M_ready is not set");
+        end
       end
+
+
 
       always @(posedge clk) begin
          if (m_ready && m_valid) begin
-            $display("y[%d] = %d" , i, data_out);
+            $display("M_valid and M_ready asserted");
+            $display("y = %d" , data_out);
             i=i+1;
          end
       end
